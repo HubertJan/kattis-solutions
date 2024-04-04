8c2cb2a31e8607e6cd6579e349a22d151e08e136
Hubert <70658889+HubertJan@users.noreply.github.com>
2024-04-04 06:55:02 +0200

    Add virtualfriends

---
+++ virtualfriends/virtualfriends.py
@@ -0,0 +1,34 @@
+import sys
+
+def track_friendships():
+    n = int(sys.stdin.readline().rstrip())
+    parents = {}
+    total_friends = {}
+    def find(x):
+        
+        if not x in parents.keys():
+            parents[x] = x
+            total_friends[x] = 1
+        if parents[x] != x:
+            parents[x] = find(parents[x])
+        return parents[x]
+    def union(x,y):
+        parents[find(y)] = find(x)
+    def friends(x):
+        return total_friends[find(x)]
+    
+    for _ in range(0, n):
+        names = sys.stdin.readline().rstrip().split(" ")
+        a, b = names[0], names[1]
+        if find(a) != find(b):
+            total_friends[find(a)] += total_friends[find(b)]
+            union(a, b)
+        print(friends(a))
+
+def main():
+    n = int(sys.stdin.readline().rstrip())
+    for _ in range(0, n):
+        track_friendships()
+
+if __name__ == "__main__":
+    main()
